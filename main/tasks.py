9a26da021bad5e70290213a41a975bf18f70ef47
sunday <sundaysdw@gmail.com>
2017-11-25 06:31:02 +0800

    Review function completed

--- main/tasks.py
+++
@@ -1,4 +0,0 @@
-from .models import myUser, Tutor, PrivateTutor, ContractTutor, Booking, Transaction, Wallet, Session, Course, Coupon, Student, Blackout, Payment,CourseCatalogue
-
-def test():
-	print "tesing..."
